b7703356c1df7666409e86f310d89af38ea91a98
dewslyse <dewslyse@gmail.com>
2022-08-02 09:48:30 +0000

    Update app routes

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,3 @@
+class ExpensesController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,27 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    unauthenticated do
+      root 'home#index', as: :unauthenticated_root
+    end
+
+    authenticated do
+      root 'categories#index', as: :user
+    end
+
+    get 'sign_in', to: 'devise/sessions#new', as: 'sign_in'
+
+    get 'sign_up', to: 'devise/registrations#new', as: 'sign_up'
+
+    get 'sign_out', to: 'devise/sessions#destroy', as: 'sign_out'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
-  root "home#index"
+  # root "home#index"
   
-  resources :users
+  # resources :users
+  resources :categories
+  resources :expenses
 end
