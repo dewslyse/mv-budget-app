2741767a128abe1f8eafe2b81bbab06a4b8962bc
dewslyse <dewslyse@gmail.com>
2022-08-03 11:58:48 +0000

    Change type for amount

--- db/migrate/20220801214735_create_expenses.rb
+++ db/migrate/20220801214735_create_expenses.rb
@@ -2,7 +2,7 @@ class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
       t.string :name
-      t.decimal :amount
+      t.float :amount
       t.belongs_to :user, index: true, foreign_key: true
 
       t.timestamps
