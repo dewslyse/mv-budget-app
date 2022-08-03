a129190ed12a9cb36a52f49a42bdb5a95226351c
dewslyse <dewslyse@gmail.com>
2022-08-03 09:44:30 +0000

    Update associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :expenses
-
+  has_many :category_expenses
+  has_many :expenses, through: :category_expenses
+  
   validates :name, presence: true
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :categories
+  has_many :category_expenses
+  has_many :categories, through: :category_expenses
 
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
